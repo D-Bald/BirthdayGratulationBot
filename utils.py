b96176aee9d9b87943f6b8ca6f3a78b8baff0abe
D-Bald <david.baldauf@web.de>
2022-06-24 22:56:34 +0200

    Date format that is used to check the input and generate the output can be changed in config.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import datetime
 import pandas as pd
 from table2ascii import table2ascii as t2a, PresetStyle
+from config import DATE_FORMAT
 
 def make_output_table(df):
     body = []
@@ -30,7 +31,7 @@ def make_output_table_for_name(df: pd.DataFrame, name: str):
     return output
 
 def check_date_format(date: str):
-    """Checks if the given date hase the date format `"%d.%m.%Y"`.
+    """Checks if the given date has the date format specified in config.py.
 
     Args:
         date: the date as string
@@ -40,7 +41,7 @@ def check_date_format(date: str):
     """
     res = True
     try:
-        res = bool(datetime.datetime.strptime(date, "%d.%m.%Y"))
+        res = bool(datetime.datetime.strptime(date, DATE_FORMAT))
     except ValueError:
         res = False
     return res
@@ -49,25 +50,25 @@ def string_to_datetime(date_str: str):
     """Parses a date string to datetime format.
 
     Args:
-        date_str: the date as string with format `"%d.%m.%Y"`
+        date_str: the date as string with format as specified in config.py
     
     Returns:
         Datetime object with given date and time "00:00:00"
     """
-    date = datetime.datetime.strptime(date_str, "%d.%m.%Y")
+    date = datetime.datetime.strptime(date_str, DATE_FORMAT)
     return date
 
 def format_date_string(date_str: str):
-    """parses a date string to string with format "dd.mm.yyy".
+    """parses a date string to string with format specified in config.py.
 
     Args:
-        date_str: the date as string with format `"%d.%m.%Y"`
+        date_str: the date as string with format like `"%d.%m.%Y"`
     
     Returns:
-        Given date as string in format "dd.mm.yyy"
+        Given date as string in format specified in config.py
     """
     date = string_to_datetime(date_str)
-    return datetime.datetime.strftime(date, "%d.%m.%Y")
+    return datetime.datetime.strftime(date, DATE_FORMAT)
 
 def _gratulation_if_zero_days_left(days_left):
     if days_left == 0:
